ba95138ca2de23b97d783f9287b36981cd9cd35a
Silviu <silviuandrei1056@gmail.com>
2024-10-28 02:40:08 +0200

    test

--- frontend/nftrealm/src/config.js
+++ frontend/nftrealm/src/config.js
@@ -1,2 +1,2 @@
-export const WALLETESCROW_ADDRESS = '0x854B5B5104a6aDEa76B8f832ba40c439818bd8d6';
+export const WALLETESCROW_ADDRESS = '0x8b388dFFa171bCAADf3386e5c83D4d654e54B5Ac';
 export const PROVIDERINFURA = 'https://sepolia.infura.io/v3/302e5352e5f243cabc272d43832be4cf';
